4445b79ab9b03f7bde98a224a27091e591179e5c
Dmitrii Zolnikov <dmitri.zol@inbox.ru>
2024-01-15 17:13:17 +0300

    Experimental Rust receiver upgraded

--- experiment/src/main.rs
+++ experiment/src/main.rs
@@ -3,9 +3,11 @@ use amqprs::{
     callbacks::{DefaultConnectionCallback, DefaultChannelCallback},
     channel::BasicConsumeArguments
 };
-use tokio::{self, sync::Notify};
+use tokio::{self};
 use tokio::io::Error as TError;
-use std::str;
+use std::{str, thread, time::Duration};
+
+const QUEUE_NAME = "hello"
 
 #[tokio::main]
 async fn main() -> Result<(), Box<TError>> {
@@ -18,33 +20,39 @@ async fn main() -> Result<(), Box<TError>> {
     .await.unwrap();
     conn.register_callback(DefaultConnectionCallback).await.unwrap();
 
-    let ch = conn.open_channel(None).await.unwrap();
-    ch.register_callback(DefaultChannelCallback).await.unwrap();
+    loop {
+        let ch = conn.open_channel(None).await.unwrap();
+        ch.register_callback(DefaultChannelCallback).await.unwrap();
 
-    let consumer_args = BasicConsumeArguments::new("hello", "some_id")
-        .auto_ack(true)
-        .finish();
+        let consumer_args = BasicConsumeArguments::new(QUEUE_NAME, "some_id")
+            .auto_ack(true)
+            .finish();
 
-    let consumer_result = ch.basic_consume_rx(consumer_args).await;
-    if consumer_result.is_ok() {
-        let (_ctag, mut rx) = consumer_result.unwrap();
+        let consumer_result = ch.basic_consume_rx(consumer_args).await;
+        if consumer_result.is_ok() {
+            let (_ctag, mut rx) = consumer_result.unwrap();
 
-        tokio::spawn(async move {
-            while let Some(msg) = rx.recv().await {
-                if let Some(payload) = msg.content {
-                    println!(" [x] Received {:?}", str::from_utf8(&payload).unwrap());
-                }
-            };
-    
-        });
+            println!(" [*] Waiting for messages. To exit press CTRL+C");
 
-        println!(" [*] Waiting for messages. To exit press CTRL+C");
-    
-        let guard = Notify::new();
-        guard.notified().await;
-    }
-    else {
-        println!("ERROR!!!")
+            let _ = tokio::spawn(async move {
+                while let Some(msg) = rx.recv().await {
+                    if let Some(payload) = msg.content {
+                        println!(" [x] Received {:?}", str::from_utf8(&payload).unwrap());
+                    }
+                };
+        
+            }).await;
+            if ch.is_open(){
+                println!("Queue removed!");
+            }else {
+                break
+            }
+        }
+        else {
+            println!("Error {:?}", consumer_result);
+            println!("Channel closed: {}", !ch.is_open());
+            thread::sleep(Duration::from_secs(5));
+        }
     }
 
     Ok(())
